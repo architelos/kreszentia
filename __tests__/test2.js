c9fc7bbe2dfbf144fc3d49878bdf39653c196edf
architelos <technopiabcde@gmail.com>
2022-10-08 20:18:57 +0700

    Add a 2nd test case

---
+++ __tests__/test2.js
@@ -0,0 +1,13 @@
+const { equal } = require("node:assert");
+
+suite("multiplication", () => {
+    add("multiply 1x1", () => {
+        equal(1 * 1, 1);
+    });
+
+    add("multiply 2x2", () => {
+        equal(2 * 2, 4);
+    });
+});
+
+run();
